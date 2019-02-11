bb3f9d37a17a6852ba285884fc332649f6454c63
muhammad11221 <muhammad98576@gmail.com>
2019-02-11 17:05:52 -0500

    Initial commit

---
+++ src/BookApp.java
@@ -0,0 +1,11 @@
+public class BookApp {
+    public static void main(String[] args) {
+
+
+    Book info = new Book("ABC", "omar", "very good book",
+        12.50, true);
+        System.out.println(info.toString());
+
+    }
+
+}
